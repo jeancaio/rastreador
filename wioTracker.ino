eb1ff119ddb8159f2656be2eed1fdc7069bb2830
jeancaio <jeanandreolla@hotmail.com>
2018-09-01 20:12:33 -0300

    ajustes

--- wioTracker.ino
+++ wioTracker.ino
@@ -3,13 +3,15 @@
 #include "MC20_GPRS.h"
 #include "MC20_GNSS.h"
 #include <Wire.h>
-
+#include <rBase64.h>
+\
 // #define OLED_OPEN
 
 #define RGBPIN 10
 #define HOST "track-j.herokuapp.com"
 #define URL_EVENT "/api/v1/posicoes/post_posicoes/"
 #define PORT 80
+#define AUTH "fb24239cefa19d5f025cceae7b63cb54"
 
 
 
@@ -19,7 +21,7 @@ GNSS gnss = GNSS();
 char body[180];
 
 const char apn[10] = "CMNET";
-const char URL[100] = "http://track-j.herokuapp.com";
+const char URL[100] = "track-j.herokuapp.com";
 char http_cmd[100] = "POST /api/v1/posicoes/post_posicoes";
 int port = 80;
 
@@ -72,19 +74,13 @@ void loop()
   if (gnss.getCoordinate())
   {
     SerialUSB.print("GNSS: ");
-    //    SerialUSB.print(gnss.longitude, 6);
-    //    SerialUSB.print(",");
-    //    SerialUSB.println(gnss.latitude, 6);
-    //    SerialUSB.print(gnss.str_longitude);
-    //    SerialUSB.print(",");
-    //    SerialUSB.println(gnss.str_latitude);
-
 
     float latitude = gnss.latitude;
     float longitude = gnss.longitude;
 
+    coordinate += F("-");
     coordinate += String(latitude, 6);
-    coordinate += F(",");
+    coordinate += F(",-");
     coordinate += String(longitude, 6);
 
     SerialUSB.println(coordinate);
@@ -94,11 +90,7 @@ void loop()
     SerialUSB.println("Error!");
   }
   postCoordiante(coordinate);
-  delay(1000);
-}
-
-void puostCoordiante(String coordinate) {
-
+  delay(2000);
 }
 
 /**
@@ -106,24 +98,25 @@ void puostCoordiante(String coordinate) {
 */
 void postCoordiante(String coordinate)
 {
-    char body[180];
+  SerialUSB.println("------POST-----");
+  char body[180];
   if (gprs.connectTCP(URL, port))
   {
     bool ret;
     char postContent[200];
 
     sprintf(body, "{\"coordenadas_geograficas\":\"John\",\"captured_at\": \"2018-01-01\"}");
-    //    sprintf(body,"%s%s", body, "\"coordenadas_geograficas\":\"" + COORDINATE + "\"");
-    //    sprintf(body,"%s%s", body, "\"}");
+
     SerialUSB.print("BODY: ");
     SerialUSB.println(body);
     /**
         Load latlng data to postContent
     */
     MC20_clean_buffer(postContent, 100);
+//    rbase64.encode(Encoding.Default.GetBytes(AUTH + ":" + ""));
 
-    sprintf(postContent, "POST %s HTTP/1.1\r\nContent-Type: application/json\r\nHost: %s\r\nContent-Length: %d\r\n\r\n%s", URL_EVENT, HOST, strlen(body), body);
-//    sprintf(postContent, "POST /api/v1/posicoes/post_posicoes/ HTTP/1.0\n\r\n\r");
+    sprintf(postContent, "POST %s HTTP/1.1\r\nContent-Type: application/json\r\nHost: %s\r\nAuthorization: Basic %s\r\nContent-Length: %d\r\n\r\n%s", URL_EVENT, HOST, rbase64.encode(AUTH), strlen(body), body);
+
     SerialUSB.print(postContent);
     gprs.sendTCPData(postContent);   // Send HTTP request
     ret = MC20_wait_for_resp("CLOSED\r\n", CMD, 10, 2000, true);
