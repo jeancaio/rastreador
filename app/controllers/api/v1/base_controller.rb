50a8652aa23400cc7e1f6539210ef59670fbb16f
jeancaio <jeanandreolla@hotmail.com>
2018-09-01 17:47:36 -0300

    debuggers

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -3,7 +3,9 @@ class Api::V1::BaseController < ActionController::API
   include ActionController::HttpAuthentication::Token::ControllerMethods
 
   def verificar_veiculo
+    puts "------BASIC AUTH-------"
     authenticate_with_http_basic do |token_veiculo, _password|
+      puts "-------PROCURANDO VEÍCULO---------"
       @veiculo = Veiculo.find_by(token_integracao: token_veiculo)
     end
     raise 'Veiculo não encontrado' unless @veiculo
--- app/controllers/api/v1/posicoes_controller.rb
+++ app/controllers/api/v1/posicoes_controller.rb
@@ -2,9 +2,11 @@ class Api::V1::PosicoesController < Api::V1::BaseController
 
   def post_posicoes
     @veiculo = verificar_veiculo
+    puts '---------VEÍCULO ENCONTRADO-------'
     @posicao = Posicao.new(coordenadas_geograficas: params[:coordenadas_geograficas], captured_at: params[:captured_at].to_datetime, veiculo: @veiculo)
 
     if @posicao.save!
+      puts '---------POST POSICAO-------'
       render json: @posicao, status: 200
     else
       render json: @posicao.errors, status: :unprocessable_entity
