1313b0fcbe5a2129c2d48caa4ea0bfd484e511d8
jeancaio <jeanandreolla@hotmail.com>
2018-09-07 15:06:59 -0300

    ajustes1

--- app/models/cliente.rb
+++ app/models/cliente.rb
@@ -3,4 +3,12 @@ class Cliente < User
   def to_s
     nome
   end
+
+  def gerar_token_integracao!
+    begin
+      self.token_integracao = SecureRandom.hex
+    end while self.class.exists?(token_integracao: token_integracao)
+
+    self.save!
+  end
 end
